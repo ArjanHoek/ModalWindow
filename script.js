3ce4b52ba141ccb8945a5b58eb5591205eb2e852
Arjan Hoek <arjanhoek5191@gmail.com>
2022-01-27 08:02:12 +0100

    add functionality for mouse and keyboard events

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+const showModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+document.querySelectorAll('.show-modal').forEach(btn => {
+  btn.addEventListener('click', showModal);
+});
+
+document.querySelector('.close-modal').addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  const close = e.key === 'Escape';
+  const isShown = !modal.classList.contains('hidden');
+  if (close && isShown) {
+    closeModal();
+  }
+});
